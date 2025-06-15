df91ca42898ad9b1f6c7200dd0c691fb15639f9e
AEMS1 <aems91806@gmail.com>
2025-06-15 13:53:42 +0330

    Add files via upload

---
+++ tokens.js
@@ -0,0 +1,32 @@
+const tokens = [
+  {
+    symbol: "BUSD",
+    address: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
+    decimals: 18
+  },
+  {
+    symbol: "WBNB",
+    address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c",
+    decimals: 18
+  },
+  {
+    symbol: "USDT",
+    address: "0x55d398326f99059fF775485246999027B3197955",
+    decimals: 18
+  },
+  {
+    symbol: "USDC",
+    address: "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d",
+    decimals: 18
+  },
+  {
+    symbol: "DAI",
+    address: "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3",
+    decimals: 18
+  },
+  {
+    symbol: "TUSD",
+    address: "0x14016e85a25aeb13065688cafb43044c2ef86784",
+    decimals: 18
+  }
+];

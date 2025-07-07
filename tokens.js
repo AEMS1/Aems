89fbc13bef61b9fb2c2c73fb0dd3f77f8a486695
AEMS1 <aems91806@gmail.com>
2025-07-07 08:08:53 +0330

    Delete tokens.js

--- tokens.js
+++
@@ -1,9 +0,0 @@
-
-const tokenList = [
-  { symbol: 'BNB', address: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee', decimals: 18 },
-  { symbol: 'BUSD', address: '0xe9e7cea3dedca5984780bafc599bd69add087d56', decimals: 18 },
-  { symbol: 'USDT', address: '0x55d398326f99059ff775485246999027b3197955', decimals: 18 },
-  { symbol: 'WBNB', address: '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c', decimals: 18 },
-  { symbol: 'CAKE', address: '0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82', decimals: 18 },
-  { symbol: 'LGD', address: '0x4751C0DE56EFB3770615097347cbF131D302498A', decimals: 18 },
-];

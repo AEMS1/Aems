8a87962510f0a0bbd848e4f42bd84cf3335b5af1
AEMS1 <aems91806@gmail.com>
2025-07-09 04:17:34 +0330

    Update tokenList.js

--- tokenList.js
+++ tokenList.js
@@ -17,6 +17,12 @@ const tokenList = [
     address: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
     decimals: 18
   },
+   {
+    symbol: "reef",
+    id: "pancakeswap-token",
+    address: "0xF21768cCBC73Ea5B6fd3C687208a7c2def2d966e",
+    decimals: 18
+  },
   {
     symbol: "ETH",
     id: "ethereum",
